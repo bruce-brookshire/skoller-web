c5ef3e6949af6e3c388d130fb9300839a53d6029
Joel Harris <joel@fortyau.com>
2017-11-22 11:50:09 -0600

    got all the sections on the landing page ready for styling

---
+++ src/views/Landing/FeatureHighlights.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+class FeatureHighlights extends React.Component {
+    render () {
+      return (
+        <div>
+            <h3>ClassNav answers all of the biggest questions you'll have throughout your semester.</h3>
+
+            <div className='row'>
+                <div>
+                    <img src='src/assets/images/landing_page/Calendar@2x.png'/>
+                </div>
+                <div>
+                    <img src='src/assets/images/landing_page/Tasks@2x.png'/>
+                </div>
+                <div>
+                    <img src='src/assets/images/landing_page/Classes@2x.png'/>
+                </div>
+                <div>
+                    <img src='src/assets/images/landing_page/Tasks@2x.png'/>
+                </div>
+            </div>         
+        </div>
+      )
+    }
+}
+
+export default FeatureHighlights
---
+++ src/views/Landing/Purpose.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+class Purpose extends React.Component {
+    render () {
+      return (
+        <div>
+            <h3>ClassNav crowdsources the classroom</h3>
+            <h6>to make it easier thn ever to stay on top of your academic life</h6>
+            <img src='src/assets/images/landing_page/iPhones_Background.png'/>
+        </div>
+      )
+    }
+}
+
+export default Purpose
