1c69352c5445fd7bb91f7d86e64952f4e180ce5f
Joel Harris <joel@fortyau.com>
2017-11-30 10:50:33 -0600

    made a speech bubble woof....i spent way longer than i should have on this. BUT DO IT FOR THE SPECS!

--- src/views/Landing/FeatureHighlights.js
+++ src/views/Landing/FeatureHighlights.js
@@ -6,20 +6,25 @@ class FeatureHighlights extends React.Component {
     return (
       <div className='even-section'>
         
-        <h3>{'ClassNav answers all of the biggest questions you\'ll have throughout your semester.'}</h3>
+        <h3 className='section-topper section-header blue-section-text'>{'ClassNav answers all of the biggest questions you\'ll have throughout your semester.'}</h3>
 
         <div className='row'>
-          <div>
-            <img src='src/assets/images/landing_page/Calendar@2x.png'/>
+          <div className='col-xs-12 col-sm-3'>
+            <h1>Filler</h1>
+            <div className='speech-bubble'>          
+              <p>This is the speech bubble</p>
+            </div>
+            <img className='highlights-images' src='src/assets/images/landing_page/Calendar@2x.png'/>
           </div>
-          <div>
-            <img src='src/assets/images/landing_page/Tasks@2x.png'/>
+          <div className='col-xs-12 col-sm-3'>
+            <img className='highlights-images' src='src/assets/images/landing_page/Tasks@2x.png'/>
           </div>
-          <div>
-            <img src='src/assets/images/landing_page/Classes@2x.png'/>
+          <div className='col-xs-12 col-sm-3'>
+            <h1>Filler</h1>
+            <img className='highlights-images' src='src/assets/images/landing_page/Classes@2x.png'/>
           </div>
-          <div>
-            <img src='src/assets/images/landing_page/Tasks@2x.png'/>
+          <div className='col-xs-12 col-sm-3'>
+            <img className='highlights-images' src='src/assets/images/landing_page/Tasks@2x.png'/>
           </div>
         </div>
       </div>
