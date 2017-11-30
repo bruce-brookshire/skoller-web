08396be75e24f90ff46d4f1f86de5798685e6c41
Joel Harris <joel@fortyau.com>
2017-11-30 15:01:44 -0600

    fixed the bubbles and made mobile view and center aligned things

--- src/views/Landing/FeatureHighlights.js
+++ src/views/Landing/FeatureHighlights.js
@@ -7,32 +7,35 @@ class FeatureHighlights extends React.Component {
       <div className='even-section'>
         
         <h3 className='section-topper section-header blue-section-text'>{'ClassNav answers all of the biggest questions you\'ll have throughout your semester.'}</h3>
-
-        <div className='row'>
-          <div className='col-xs-12 col-sm-3'>
-            <h1>Filler</h1>
-            <div className='speech-bubble'>          
-              <p>{'When\'s the best weekend to go on that big road trip?'}</p>
+        <div className='section-header'>
+          <div className='row'>
+            <div className='col-xs-6 col-sm-3'>
+              <div className='speech-bubble'>          
+                <p>{'When\'s the best weekend to go on that big road trip?'}</p>
+                <div className='triangle-inner' />
+              </div>
+              <img className='highlights-images' src='src/assets/images/landing_page/Calendar@2x.png'/>
             </div>
-            <img className='highlights-images' src='src/assets/images/landing_page/Calendar@2x.png'/>
-          </div>
-          <div className='col-xs-12 col-sm-3'>
-            <img className='highlights-images' src='src/assets/images/landing_page/Tasks@2x.png'/>
-            <div className='speech-bubble invert'>          
-              <p className='invert'>{'What\'s most important to work on today?'}</p>
+            <div className='col-xs-6 col-sm-3'>
+              <img className='highlights-images' src='src/assets/images/landing_page/Tasks@2x.png'/>
+              <div className='speech-bubble invert'>          
+                <p className='invert'>{'What\'s most important to work on today?'}</p>
+                <div className='triangle-inner' />
+              </div>
             </div>
-          </div>
-          <div className='col-xs-12 col-sm-3'>
-            <h1>Filler</h1>
-            <div className='speech-bubble'>          
-              <p>How am I doing in all my classes?</p>
+            <div className='col-xs-6 col-sm-3'>
+              <div className='speech-bubble'>          
+                <p>How am I doing in all my classes?</p>
+                <div className='triangle-inner' />
+              </div>
+              <img className='highlights-images' src='src/assets/images/landing_page/Classes@2x.png'/>
             </div>
-            <img className='highlights-images' src='src/assets/images/landing_page/Classes@2x.png'/>
-          </div>
-          <div className='col-xs-12 col-sm-3'>
-            <img className='highlights-images' src='src/assets/images/landing_page/Tasks@2x.png'/>
-            <div className='speech-bubble invert'>          
-              <p className='invert'>What do I need to make on the rest of my assignments to get the grade I want?</p>
+            <div className='col-xs-6 col-sm-3'>
+              <img className='highlights-images' src='src/assets/images/landing_page/Tasks@2x.png'/>
+              <div className='speech-bubble invert'>          
+                <p className='invert'>What do I need to make on the rest of my assignments to get the grade I want?</p>
+                <div className='triangle-inner' />
+              </div>
             </div>
           </div>
         </div>
