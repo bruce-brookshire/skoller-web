c28b861c8b7ae9ddcf5e7cc08a7967bc405844cb
Camden <camden@fortyau.com>
2017-12-05 17:39:06 -0600

    Connect backend to navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import {observer} from 'mobx-react'
+import {inject, observer} from 'mobx-react'
 import {Link} from 'react-router'
 
+@inject('rootStore') @observer
 class NavBar extends React.Component {
   render () {
+    const {userStore: {user}} = this.props.rootStore
+    console.log(user)
     return (
       <div className='cn-navbar'>
         <div className='left'>
@@ -11,8 +14,8 @@ class NavBar extends React.Component {
         </div>
         <div className='user-info right'>
           <div className='left'>
-            <p>Jonathan Rankin</p>
-            <span>Belmont University</span>
+            <p>{user.student.name_first} {user.student.name_last}</p>
+            <span>{user.student.school.name}</span>
           </div>
           <div className='right'>
             <img className='profile-img' src='https://www.biography.com/.image/c_fill%2Ccs_srgb%2Cg_face%2Ch_300%2Cq_80%2Cw_300/MTE5NDg0MDU0NTIzODQwMDE1/steven-jobs-9354805-2-402.jpg'/>
