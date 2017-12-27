9fa04772b823bcac529f9d25096f892e743bc126
duane <duane@fortyau.com>
2017-12-27 17:15:59 -0600

    Responsive and speech bubble design updates

--- src/views/Landing/FeatureHighlights.js
+++ src/views/Landing/FeatureHighlights.js
@@ -1,40 +1,42 @@
 import React from 'react'
-import Slant from '../../components/Slant'
 
 class FeatureHighlights extends React.Component {
+  /**
+   * @TODO refactor away utility classes
+   */
   render () {
     return (
       <div className='section-features'>
         <h2 className='section-header'>
           Skoller answers all of the biggest questions you'll have throughout your semester.
         </h2>
-        <div className='row'>
-          <div className='col-xs-6 col-sm-3'>
+        <div className='row features'>
+          <div className='feature col-xs-6 col-sm-3'>
             <div className='speech-bubble'>
               <p>{'When\'s the best weekend to go on that big road trip?'}</p>
-              <div className='triangle-inner' />
+              <div className="triangle"></div>
             </div>
             <img className='highlights-images' src='src/assets/images/landing_page/Calendar@2x.png'/>
           </div>
-          <div className='col-xs-6 col-sm-3'>
+          <div className='feature col-xs-6 col-sm-3'>
             <img className='highlights-images' src='src/assets/images/landing_page/Tasks@2x.png'/>
-            <div className='speech-bubble invert'>
-              <p className='invert'>{'What\'s most important to work on today?'}</p>
-              <div className='triangle-inner' />
+            <div className='speech-bubble'>
+              <p>{'What\'s most important to work on today?'}</p>
+              <div className="triangle"></div>
             </div>
           </div>
-          <div className='col-xs-6 col-sm-3'>
+          <div className='feature col-xs-6 col-sm-3'>
             <div className='speech-bubble'>
               <p>How am I doing in all my classes?</p>
-              <div className='triangle-inner' />
+              <div className="triangle"></div>
             </div>
             <img className='highlights-images' src='src/assets/images/landing_page/Classes@2x.png'/>
           </div>
-          <div className='col-xs-6 col-sm-3'>
+          <div className='feature col-xs-6 col-sm-3'>
             <img className='highlights-images' src='src/assets/images/landing_page/Tasks@2x.png'/>
-            <div className='speech-bubble invert'>
-              <p className='invert'>What do I need to make on the rest of my assignments to get the grade I want?</p>
-              <div className='triangle-inner' />
+            <div className='speech-bubble'>
+              <p>What do I need to make on the rest of my assignments to get the grade I want?</p>
+              <div className="triangle"></div>
             </div>
           </div>
         </div>
