ac463563f00d435a255466cb91a587d1c4a951f8
duane <duane@fortyau.com>
2017-12-26 22:10:07 -0600

    Style process and register components

--- src/views/Landing/Purpose.js
+++ src/views/Landing/Purpose.js
@@ -1,20 +1,17 @@
 import React from 'react'
-import Slant from '../../components/Slant'
 
 class Purpose extends React.Component {
   render () {
     return (
-      <div className='horizontal-align-row center'>
-        <div className='section-dimensions blue-section-text even-section'>
-          <Slant />
-          <div className="section-header">
-            <span className='blue-section-text section-text section-topper'>Skoller crowdsources the classroom</span>
-            <span className='blue-section-text section-tagline'>to make it easier than ever to stay on top of your academic life</span>
-          </div>
-          <div className="center-text">
-            <img className='purpose_image' src='src/assets/images/landing_page/iPhones_Background.png'/>
-          </div>
-          {/* <Slant /> */}
+      <div className="section-purpose">
+        <h2 className="section-header">
+          Skoller crowdsources the classroom
+          <span className="muted">
+            to make it easier than ever to stay on top of your academic life
+          </span>
+        </h2>
+        <div className="marketing-img">
+          <img className='img-purpose' src='src/assets/images/landing_page/iPhones_Background.png'/>
         </div>
       </div>
     )
