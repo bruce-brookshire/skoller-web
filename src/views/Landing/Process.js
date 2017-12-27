dec3a683a76ffe6b4d08ab9afc513278b4e4f114
duane <duane@fortyau.com>
2017-12-26 22:33:58 -0600

    Process section styling

--- src/views/Landing/Process.js
+++ src/views/Landing/Process.js
@@ -1,48 +1,43 @@
 import React from 'react'
-import Slant from '../../components/Slant'
 
 class Process extends React.Component {
   render () {
     return (
-      <div className='shift-up vertical-align center'>
-        <Slant className='no-bg white-fill'/>
-        <div className="page-height vertical-align center">
-          <h2 className='center-text'>{'Here\'s how it works'}</h2>
-          <div className='row section-header'>
-            <div className='col-xs-12 col-sm-4 center-text'>
-              <img className='process-icon-size' src='src/assets/images/landing_page/step_one.png'/>
-              <h4>
-                1. Create and account
-              </h4>
-              <p className='muted-text'>
-                Create an account using your school email and select your classes.
-              </p>
-
-            </div>
-            <div className='col-xs-12 col-sm-4 center-text'>
-              <img className='process-icon-size' src='src/assets/images/landing_page/step_two.png'/>
-              <h4>
-                2. Upload your syllabi
-              </h4>
-              <p className='muted-text'>
-                Drop a syllabus for each of your classes on our website.
-              </p>
-            </div>
-            <div className='col-xs-12 col-sm-4 center-text'>
-              <img className='process-icon-size' src='src/assets/images/landing_page/step_three.png'/>
-              <h4>
-                3. Relax
-              </h4>
-              <p className='muted-text'>
-                Forget the stress while the important information on your syllabus is extracted seamlessly.
-              </p>
-            </div>
-          </div>
-          <div>
-            <h4 className='center-text blue-text'>
-              When the syllabus process is finished, just open the app and find everything already in place.
-            </h4>
-          </div>
+      <div className='section-process'>
+        <h2 className="section-header">Here's how it works</h2>
+        <ul className='row process-list'>
+          <li className='col-xs-12 col-sm-4 center-text'>
+            <img className='process-icon-size' src='src/assets/images/landing_page/step_one.png'/>
+            <h3>
+              1. Create and account
+            </h3>
+            <p className='muted-text'>
+              Create an account using your school email and select your classes.
+            </p>
+          </li>
+          <li className='col-xs-12 col-sm-4 center-text'>
+            <img className='process-icon-size' src='src/assets/images/landing_page/step_two.png'/>
+            <h3>
+              2. Upload your syllabi
+            </h3>
+            <p className='muted-text'>
+              Drop a syllabus for each of your classes on our website.
+            </p>
+          </li>
+          <li className='col-xs-12 col-sm-4 center-text'>
+            <img className='process-icon-size' src='src/assets/images/landing_page/step_three.png'/>
+            <h3>
+              3. Relax
+            </h3>
+            <p className='muted-text'>
+              Forget the stress while the important information on your syllabus is extracted seamlessly.
+            </p>
+          </li>
+        </ul>
+        <div>
+          <p className='content'>
+            When the syllabus process is finished, just open the app and find everything already in place.
+          </p>
         </div>
       </div>
     )
