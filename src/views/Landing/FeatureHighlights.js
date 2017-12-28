50dfc59c5fc1d9dbcd88a20bb242aced6d5eb422
duane <duane@fortyau.com>
2017-12-28 12:07:11 -0600

    Fix feature stagger

--- src/views/Landing/FeatureHighlights.js
+++ src/views/Landing/FeatureHighlights.js
@@ -11,29 +11,29 @@ class FeatureHighlights extends React.Component {
           Skoller answers all of the biggest questions you'll have throughout your semester.
         </h2>
         <div className='row features'>
-          <div className='feature col-xs-6 col-sm-3'>
+          <div className='feature'>
             <div className='speech-bubble'>
               <p>{'When\'s the best weekend to go on that big road trip?'}</p>
               <div className="triangle"></div>
             </div>
-            <img className='highlights-images' src='src/assets/images/landing_page/Calendar@2x.png'/>
+            <img src='src/assets/images/landing_page/Calendar@2x.png'/>
           </div>
-          <div className='feature col-xs-6 col-sm-3'>
-            <img className='highlights-images' src='src/assets/images/landing_page/Tasks@2x.png'/>
+          <div className='feature'>
+            <img src='src/assets/images/landing_page/Tasks@2x.png'/>
             <div className='speech-bubble'>
               <p>{'What\'s most important to work on today?'}</p>
               <div className="triangle"></div>
             </div>
           </div>
-          <div className='feature col-xs-6 col-sm-3'>
+          <div className='feature'>
             <div className='speech-bubble'>
               <p>How am I doing in all my classes?</p>
               <div className="triangle"></div>
             </div>
-            <img className='highlights-images' src='src/assets/images/landing_page/Classes@2x.png'/>
+            <img src='src/assets/images/landing_page/Classes@2x.png'/>
           </div>
-          <div className='feature col-xs-6 col-sm-3'>
-            <img className='highlights-images' src='src/assets/images/landing_page/Tasks@2x.png'/>
+          <div className='feature'>
+            <img src='src/assets/images/landing_page/Tasks@2x.png'/>
             <div className='speech-bubble'>
               <p>What do I need to make on the rest of my assignments to get the grade I want?</p>
               <div className="triangle"></div>
