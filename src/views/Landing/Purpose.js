d96fd4d4a62d1bf5b4b1c1ebb9b479e65ea48d16
duane <duane@fortyau.com>
2017-12-28 12:50:13 -0600

    Fix purpose on large screens

--- src/views/Landing/Purpose.js
+++ src/views/Landing/Purpose.js
@@ -4,12 +4,14 @@ class Purpose extends React.Component {
   render () {
     return (
       <div className="section-purpose">
-        <h2 className="section-header">
-          Skoller crowdsources the classroom
-          <span className="muted">
-            to make it easier than ever to stay on top of your academic life
-          </span>
-        </h2>
+        <div className="content">
+          <h2 className="section-header">
+            Skoller crowdsources the classroom
+            <span className="muted">
+              to make it easier than ever to stay on top of your academic life
+            </span>
+          </h2>
+        </div>
         <div className="marketing-img">
           <img className='img-purpose' src='src/assets/images/landing_page/iPhones_Background.png'/>
         </div>
