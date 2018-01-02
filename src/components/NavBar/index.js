1cdc3a101c96e44b8eec4cbe69ef37d5d623ac31
Camden <camden@fortyau.com>
2018-01-02 14:27:23 -0600

    Add in intiials for profile pic

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -39,29 +39,21 @@ class NavBar extends React.Component {
     }
   }
 
-  determineProfileLogo () {
-    const {userStore: {user}} = this.props.rootStore
-    if (user.avatar != null){
-      return true
-    }
-    else {
-      return false
-    }
-  }
-
-  mapRoles (r){
-    return r.name
-  }
-
   getInitials () {
     const {userStore: {user}} = this.props.rootStore
-    var role_names = user.roles.map(this.mapRoles);
-    if (role_names.indexOf("Admin") != -1){
+    var roleNames = user.roles.map((r) => r.name)
+    if (roleNames.indexOf('Admin') !== -1) {
       return 'AD'
-    } else if (role_names.indexOf("Syllabus Worker") != -1) {
+    } else if (roleNames.indexOf('Syllabus Worker') !== -1) {
       return 'SW'
-    } else if (role_names.indexOf("Student") != -1){
-      return user.student.name_first[0].toUpperCase() + user.student.name_last[0].toUpperCase()
+    } else if (roleNames.indexOf('Student') !== -1) {
+      if (user.student.name_first && user.student.name_last) {
+        return user.student.name_first[0].toUpperCase() + user.student.name_last[0].toUpperCase()
+      } else if (user.student.name_first.length >= 2)
+        return user.student.name_first.substring(0,2).toUpperCase()
+      } else {
+        return 'ST'
+      }
     }
   }
 
@@ -78,7 +70,9 @@ class NavBar extends React.Component {
             <span>{this.getDescription()}</span>
           </div>
           <div className='right'>
-            {this.determineProfileLogo() ? <img className='profile-img' src={this.user.avatar}/> : <div className='profile-img vertical-align profile-initials'>{this.getInitials()}</div>}
+            {user.avatar ?
+              <img className='profile-img' src={this.user.avatar}/> :
+              <div className='profile-img vertical-align profile-initials'>{this.getInitials()}</div>}
           </div>
         </div>
       </div>
