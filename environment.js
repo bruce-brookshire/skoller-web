5fb1a2b1fa8c8f09fd8c21d4f79ef89edb108415
Camden <camden@fortyau.com>
2018-01-05 18:57:15 -0600

    Switch env over to production

--- environment.js
+++ environment.js
@@ -3,7 +3,7 @@ var _Environments = {
     SERVER_NAME: 'http://localhost:4000'
   },
   staging: {
-    SERVER_NAME: 'https://api.skoller.co'
+    SERVER_NAME: 'https://api-staging.skoller.co'
   },
   production: {
     SERVER_NAME: 'https://api.skoller.co'
@@ -13,7 +13,7 @@ var _Environments = {
 function getEnvironment () {
   // Insert logic here to get the current platform (e.g. staging, production, etc)
   // var platform = getPlatform()
-  var platform = 'staging'
+  var platform = 'production'
 
   // ...now return the correct environment
   return _Environments[platform]
