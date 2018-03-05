96bf90f28f60b51dabb937c0ad18611d5a4f97a0
Tyler Witt <tyler@fortyau.com>
2018-03-05 15:55:12 -0600

    Syllabus processing analytics

--- src/views/Hub/Analytics/SyllabiProcessing.js
+++ src/views/Hub/Analytics/SyllabiProcessing.js
@@ -39,40 +39,53 @@ class SyllabiProcessing extends React.Component {
     const {data} = this.props
     return (
       <div>
-        <ul>
-          <li>
-            <i className='fa fa-info general-field-info-icon'></i>
-            <strong>Classes available:</strong>
+        <div className="cn-analytics-list">
+          <div>
+            <span className="cn-analytics-label"><strong>Classes available: </strong></span>
             <span>{data.class_count}</span>
-          </li>
-        </ul>
+          </div>
+          <div>
+            <span className="cn-analytics-label"><strong>Classes with enrollment: </strong></span>
+            <span>{data.enrollment}</span>
+          </div>
+          <div>
+            <span className="cn-analytics-label"><strong>Classes with syllabi submitted: </strong></span>
+            <span>{data.class_syllabus_count} ({(data.class_syllabus_count / data.class_count) * 100} %)</span>
+          </div>
+          <div>
+            <span className="cn-analytics-label"><strong>Completed classes: </strong></span>
+            <span>{data.completed_class} </span><span className="cn-red">({data.class_in_review} in review)</span>
+            <ul>
+              <li>
+                <span className="cn-analytics-sub-label"><strong>Completed by Skoller: </strong></span>
+                <span>{data.completed_by_skoller} ({(data.completed_by_skoller / data.completed_class) * 100} %)</span>
+              </li>
+              <li>
+                <span className="cn-analytics-sub-label"><strong>Completed by DIY: </strong></span>
+                <span>{data.completed_by_diy} ({(data.completed_by_diy / data.completed_class) * 100} %)</span>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <span className="cn-analytics-label"><strong>Communities: </strong></span>
+            <span>{data.communitites} ({(data.communitites / data.enrollment) * 100} %)</span>
+          </div>
+          <div>
+            <span className="cn-analytics-label"><strong>Student created classes: </strong></span>
+            <span>{data.student_created_classes} ({(data.student_created_classes / data.class_count) * 100} %)</span>
+          </div>
+          <div>
+            <span className="cn-analytics-label"><strong>Classes with multiple files: </strong></span>
+            <span>{data.classes_multiple_files} ({(data.classes_multiple_files / data.class_syllabus_count) * 100} %)</span>
+          </div>
+          <div>
+            <span className="cn-analytics-label"><strong>Classes per student (avg.): </strong></span>
+            <span>{data.avg_classes}</span>
+          </div>
+        </div>
       </div>
     )
   }
-
-  // Classes available: 37,392
-  // Classes with enrollment: 28,403
-  // Classes with syllabi submitted: 25,038 (87.53%)
-  // Completed classes: 25,014 (24 in review)
-  // Completed by Skoller: 19,847 (76.82%)
-  // Completed by DIY: 5,139 (24.47%)
-  // Communities: 24,877 (98%)
-  // Student created classes: 201 (1%)
-  // Classes with multiple files: 821 (2.5%)
-  // Classes per student (avg.): 4.8
-
-  // student_created_classes	0
-  // enrollment	1
-  // completed_class	1
-  // completed_by_skoller	1
-  // completed_by_diy	0
-  // communitites	1
-  // classes_multiple_files	0
-  // class_syllabus_count	7
-  // class_in_review	7
-  // class_count	8
-  // avg_classes	1
-  
 }
 
 SyllabiProcessing.propTypes = {
