d991e2dc303301b7e0deb23e38f6f7aa1be49fdf
Tyler Witt <tyler@fortyau.com>
2018-03-30 09:49:15 -0500

    Made booleans visible on table.

--- src/views/Hub/Switchboard/AssignmentReminders.js
+++ src/views/Hub/Switchboard/AssignmentReminders.js
@@ -18,13 +18,31 @@ const headers = [
 ]
 
 class AssignmentReminders extends React.Component {
+  mapRow (item, index) {
+    const {message, is_plural, is_today} = item
+
+    const row = {
+      message: message,
+      is_plural: is_plural ? 'Yes' : 'No',
+      is_today: is_today ? 'Yes' : 'No'
+    }
+    return row
+  }
+
+  getRows () {
+    const {reminders} = this.props
+    return reminders.map((item, index) =>
+      this.mapRow(item, index)
+    )
+  }
+
   render () {
     return (
       <div className='cn-log-table'>
         <Grid
           className='striped'
           headers={headers}
-          rows={this.props.reminders}
+          rows={this.getRows()}
           disabled={true}
           canDelete={false}
           canSelect={false}
