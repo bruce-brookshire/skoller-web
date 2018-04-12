4b2b08bbe9fdd40cb5d785286158072d3cdc67e6
Tyler Witt <tyler@fortyau.com>
2018-04-12 10:18:10 -0500

    Added new assets to landing.

--- src/views/Landing/LandingMessage.js
+++ src/views/Landing/LandingMessage.js
@@ -4,15 +4,27 @@ class PromoSignup extends React.Component {
   render () {
     return (
       <div className='container-landing-message'>
-        <div className='container-form-register'>
-          <div id='sign-up-form-container'>
-            <div id='sign-up-form'>
-              <form className='form-padding'>
-                <h2>Keep up with your classes, together.</h2>
-              </form>
-            </div>
-          </div>
-        </div>
+        <h2>Keep up with your classes, together.</h2>
+        <ul className='landing-message'>
+          <li className='message-row'>
+            <img className='cn-person' src='src/assets/images/landing_page/dudley.png' />
+            Reminders for every assignment
+          </li>
+          <li className='message-break'>
+            <img src='src/assets/images/landing_page/squiggle1.png' />
+          </li>
+          <li className='message-row'>
+            <img className='cn-matilda' src='src/assets/images/landing_page/matilda.png' />
+            Chat with all your classmates
+          </li>
+          <li className='message-break'>
+            <img src='src/assets/images/landing_page/squiggle2.png' />
+          </li>
+          <li className='message-row'>
+            <img className='cn-person' src='src/assets/images/landing_page/peggy.png' />
+            Private and student-powered
+          </li>
+        </ul>
       </div>
     )
   }
