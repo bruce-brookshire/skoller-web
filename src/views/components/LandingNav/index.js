dba78497a139dc6da0e03b3683f2bb4dcf7b00f7
Tyler Witt <tyler@fortyau.com>
2018-04-12 17:23:15 -0500

    ET phone home

--- src/views/components/LandingNav/index.js
+++ src/views/components/LandingNav/index.js
@@ -7,7 +7,7 @@ class LandingNav extends React.Component {
       <div className='cn-landing-navbar'>
         <div className='cn-navbar-content'>
           <h1>
-            <img alt="Skoller" className='logo' src='src/assets/images/logo-wide-blue@1x.png' />
+            <a href='https://www.skoller.co/landing' ><img alt="Skoller" className='logo' src='src/assets/images/logo-wide-blue@1x.png' /></a>
           </h1>
           <LoginForm rootStore={this.props.rootStore}/>
         </div>
