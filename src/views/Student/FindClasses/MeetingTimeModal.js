b7e520f0f20f6ff6d508105f6de6dd5385853956
Tyler Witt <tyler@fortyau.com>
2018-04-26 13:43:20 -0500

    Hide days and times if online.

--- src/views/Student/FindClasses/MeetingTimeModal.js
+++ src/views/Student/FindClasses/MeetingTimeModal.js
@@ -87,6 +87,60 @@ class MeetingTimeModal extends React.Component {
     // }
   }
 
+  renderTimes () {
+    const {form} = this.state
+    const {formErrors, updateProperty} = this.props
+    return (
+      <div>
+        <div className='cn-meeting-time-label'>Meet time</div>
+        <div className='cn-meeting-times'>
+          <TimeInputField
+            containerClassName='cn-meeting-time'
+            error={formErrors.meet_time_hour}
+            name='meet_time_hour'
+            onBlur={() => {
+              if (form.meet_time_hour.length < 2) {
+                let newForm = form
+                newForm.meet_time_hour = form.meet_time_hour.padStart(2, '0')
+                this.setState({form: newForm})
+              }
+            }}
+            onChange={(updateProperty)}
+            value={form.meet_time_hour}
+            type='hour'
+          />
+          <div className='cn-meeting-time'>:</div>
+          <TimeInputField
+            containerClassName='cn-meeting-time'
+            error={formErrors.meet_time_min}
+            name='meet_time_min'
+            onBlur={() => {
+              let newForm = form
+              if (newForm.meet_time_min[1] % 5 !== 0) {
+                newForm.meet_time_min = (5 * Math.round(newForm.meet_time_min / 5)).toString()
+              }
+              if (newForm.meet_time_min.length < 2) {
+                newForm.meet_time_min = newForm.meet_time_min.padStart(2, '0')
+              }
+              this.setState({form: newForm})
+            }}
+            onChange={updateProperty}
+            value={form.meet_time_min}
+            type='min'
+          />
+          <SelectField
+            containerClassName='cn-meeting-time-select'
+            error={formErrors.meet_time_ampm}
+            name='meet_time_ampm'
+            onChange={updateProperty}
+            value={form.meet_time_ampm}
+            options={ampm}
+          />
+        </div>
+      </div>
+    )
+  }
+
   render () {
     const {form, universityError} = this.state
     const {formErrors, updateProperty} = this.props
@@ -105,55 +159,11 @@ class MeetingTimeModal extends React.Component {
               value={form.is_online}
             />
           </div>
-          <div className='cn-meeting-time-label'>Meet days</div>
-          <div className='cn-meeting-time-days'>
+          {!form.is_online && <div className='cn-meeting-time-label'>Meet days</div>}
+          {!form.is_online && <div className='cn-meeting-time-days'>
             {this.renderDays()}
-          </div>
-          <div className='cn-meeting-time-label'>Meet time</div>
-          <div className='cn-meeting-times'>
-            <TimeInputField
-              containerClassName='cn-meeting-time'
-              error={formErrors.meet_time_hour}
-              name='meet_time_hour'
-              onBlur={() => {
-                if (form.meet_time_hour.length < 2) {
-                  let newForm = form
-                  newForm.meet_time_hour = form.meet_time_hour.padStart(2, '0')
-                  this.setState({form: newForm})
-                }
-              }}
-              onChange={(updateProperty)}
-              value={form.meet_time_hour}
-              type='hour'
-            />
-            <div className='cn-meeting-time'>:</div>
-            <TimeInputField
-              containerClassName='cn-meeting-time'
-              error={formErrors.meet_time_min}
-              name='meet_time_min'
-              onBlur={() => {
-                let newForm = form
-                if (newForm.meet_time_min[1] % 5 !== 0) {
-                  newForm.meet_time_min = (5 * Math.round(newForm.meet_time_min / 5)).toString()
-                }
-                if (newForm.meet_time_min.length < 2) {
-                  newForm.meet_time_min = newForm.meet_time_min.padStart(2, '0')
-                }
-                this.setState({form: newForm})
-              }}
-              onChange={updateProperty}
-              value={form.meet_time_min}
-              type='min'
-            />
-            <SelectField
-              containerClassName='cn-meeting-time-select'
-              error={formErrors.meet_time_ampm}
-              name='meet_time_ampm'
-              onChange={updateProperty}
-              value={form.meet_time_ampm}
-              options={ampm}
-            />
-          </div>
+          </div>}
+          {!form.is_online && this.renderTimes()}
         </form>
       </div>
     )
