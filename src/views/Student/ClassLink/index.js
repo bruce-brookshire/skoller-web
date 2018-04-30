ff9087555b74ad4905d9261f831a1ace728808a9
Tyler Witt <tyler@fortyau.com>
2018-04-30 10:34:27 -0500

    Centered the freakin link.

--- src/views/Student/ClassLink/index.js
+++ src/views/Student/ClassLink/index.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+const enrollmentLink = 'localhost:8080/e/4erkl'
+const className = 'Business Law'
+
 class ClassLink extends React.Component {
   render () {
     return (
-      <div>
+      <div className='cn-class-link-container'>
+        <div className='cn-class-link-header'>
+          {className} is live! ⚡️
+        </div>
+        <div className='cn-class-link-text'>
+          Now it&apos;s time to grow the community. Click on the box below to copy the sign up link, then send it to your classmates!
+        </div>
+        <div className='cn-class-link-background'>
+          <div className='cn-class-link'>
+            {enrollmentLink}
+          </div>
+        </div>
       </div>
     )
   }
 }
+
+
 
 export default ClassLink
