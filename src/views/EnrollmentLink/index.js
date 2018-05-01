85b774bc77705226425614c7fb60d84673324f35
Tyler Witt <tyler@fortyau.com>
2018-05-01 09:04:38 -0500

    enrollment link landing page styled and mostly implemented.

--- src/views/EnrollmentLink/index.js
+++ src/views/EnrollmentLink/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import actions from '../../actions'
+import LandingNav from '../components/LandingNav'
 
 class EnrollmentLink extends React.Component {
   constructor (props) {
@@ -21,15 +22,37 @@ class EnrollmentLink extends React.Component {
   }
 
   render () {
+    const {linkDetail} = this.state
     return (
-      <div>
+      <div className='cn-enrollment-link-container'>
+        <LandingNav
+          rootStore={this.props.rootStore}
+          noLogin={true}
+          imgPath='../src/assets/images/logo-wide-blue@1x.png'
+        />
+        <div className='cn-enrollment-link-content'>
+          <img className='cn-enrollment-link-pic' src='../src/assets/images/lilpeople.png' />
+          <div className='cn-enrollment-link-content-inner'>
+            {linkDetail && <div className='cn-enrollment-link-msg'>
+              {linkDetail.student_name_first} has invited you to join {linkDetail.class_name}!
+            </div>}
+            <div>
+              Skoller makes it easy for you and your classmates to collaborate,
+              chat, and keep up with academic life.
+            </div>
+            <button className='button full-width margin-top'>
+              Join Class
+            </button>
+          </div>
+        </div>
       </div>
     )
   }
 }
 
 EnrollmentLink.PropTypes = {
-  params: PropTypes.object.isRequired
+  params: PropTypes.object.isRequired,
+  rootStore: PropTypes.object
 }
 
 export default EnrollmentLink
--- src/views/components/LandingNav/index.js
+++ src/views/components/LandingNav/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import LoginForm from './LoginForm'
+import PropTypes from 'prop-types'
 import {browserHistory} from 'react-router'
 
 class LandingNav extends React.Component {
@@ -8,12 +9,19 @@ class LandingNav extends React.Component {
       <div className='cn-landing-navbar'>
         <div className='cn-navbar-content'>
           <h1>
-            <a onClick={() => { browserHistory.push('/landing'); window.scrollTo(0, 0) }} ><img alt="Skoller" className='logo' src='src/assets/images/logo-wide-blue@1x.png' /></a>
+            <a onClick={() => { browserHistory.push('/landing'); window.scrollTo(0, 0) }} ><img alt="Skoller" className='logo' src={this.props.imgPath || 'src/assets/images/logo-wide-blue@1x.png'} /></a>
           </h1>
-          <LoginForm rootStore={this.props.rootStore}/>
+          {!this.props.noLogin && <LoginForm rootStore={this.props.rootStore}/>}
         </div>
       </div>
     )
   }
 }
+
+LandingNav.PropTypes = {
+  rootStore: PropTypes.object,
+  noLogin: PropTypes.bool,
+  imgPath: PropTypes.string
+}
+
 export default LandingNav
