4508db44ce1f7f48077a9cb8cf8b582ab44f888c
Tyler Witt <tyler@fortyau.com>
2018-05-04 14:23:02 -0500

    Styled title.

--- src/components/ClassCard/index.js
+++ src/components/ClassCard/index.js
@@ -3,11 +3,14 @@ import PropTypes from 'prop-types'
 
 class ClassCard extends React.Component {
 
-
   render () {
+    const {cl} = this.props
+
     return (
       <div className='cn-class-card'>
-        STOP
+        <div className='cn-class-title'>
+          {cl.name}
+        </div>
       </div>
     )
   }
