df3653679f3fbca99bdaeae630815f57db42a5c6
Tyler Witt <tyler@fortyau.com>
2018-05-11 16:28:00 -0500

    Moar linting.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,10 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import {inject, observer} from 'mobx-react'
-// import Loading from '../components/Loading'
 import Snackbar from '../components/Snackbar'
 
 @inject('rootStore') @observer
 class App extends React.Component {
-  componentWillMount () {
-    // const {navigationStore} = this.props.rootStore
-    // navigationStore.logout = () => { browserHistory.push('/') }
-  }
-
   renderSnackBar () {
     const {snackbarStore} = this.props.rootStore
     return <Snackbar message={snackbarStore.message} show={snackbarStore.show} type={snackbarStore.type}/>
