dd077a66399f24b97c0ac6a03f2ed94b69c7b94a
Steven T. Norris <steven@fortyau.com>
2019-01-24 15:25:48 -0600

    -Adjust opacity transitions

--- src/views/Landing/LandingMessage.js
+++ src/views/Landing/LandingMessage.js
@@ -12,9 +12,11 @@ class LandingMessage extends React.Component {
   render () {
     return (
       <div className='container-landing-message'>
+        <div className='image-container-landing-message'>
         <img src={require('../../assets/images/landing_collaborate.png')} className='image-landing-message' />
         <img src={require('../../assets/images/landing_keepup.png')} className='image-landing-message' />
         <img src={require('../../assets/images/landing_organize.png')} className='image-landing-message' />
+        </div>
       </div>
     )
   }
