5c5d55728bcedebc0c275c6a6e8252ffd2ad7020
Steven T. Norris <steven@fortyau.com>
2019-01-25 16:51:00 -0600

    -Fixes for landing images

--- src/views/Landing/LandingMessage.js
+++ src/views/Landing/LandingMessage.js
@@ -12,10 +12,25 @@ class LandingMessage extends React.Component {
   render () {
     return (
       <div className='container-landing-message'>
-        <div className='image-container-landing-message'>
-        <img src={require('../../assets/images/landing_collaborate.png')} className='image-landing-message' />
-        <img src={require('../../assets/images/landing_keepup.png')} className='image-landing-message' />
-        <img src={require('../../assets/images/landing_organize.png')} className='image-landing-message' />
+        <div className='landing-image-carousel'>
+          <div className='landing-image-carousel-container'>
+            <div className='landing-image-container'>
+              <img src={require('../../assets/images/landing_collaborate.png')} className='landing-image' />
+            </div>
+            <div className='landing-image-text'>A better way for students to <b>Collaborate</b></div>
+          </div>
+          <div className='landing-image-carousel-container'>
+            <div className='landing-image-container'>
+              <img src={require('../../assets/images/landing_organize.png')} className='landing-image' />
+            </div>
+            <div className='landing-image-text'>A better way for students to <b>Organize</b></div>
+          </div>
+          <div className='landing-image-carousel-container'>
+            <div className='landing-image-container'>
+              <img src={require('../../assets/images/landing_keepup.png')} className='landing-image' />
+            </div>
+            <div className='landing-image-text'>A better way for students to <b>Keep Up</b></div>
+          </div>
         </div>
       </div>
     )
