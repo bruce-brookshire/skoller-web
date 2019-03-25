a9c3cd1958e6e470d53078d7926af564d4c83a7b
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2019-03-25 12:18:13 -0500

    changed blog button to point to blog

--- src/views/components/LandingFooter/index.js
+++ src/views/components/LandingFooter/index.js
@@ -6,7 +6,7 @@ class LandingFooter extends React.Component {
       <footer className='cn-landing-footer'>
         <div className='footer-content'>
           <div className='footer-link'>
-            <a className='non-styled-link' href='https://explore.skoller.co'>Blog</a>
+            <a className='non-styled-link' href='https://explore.skoller.co/blog'>Blog</a>
           </div>
           <div className='footer-link'>
             <a className='non-styled-link' href='https://explore.skoller.co/press'>What people are saying about us</a>
