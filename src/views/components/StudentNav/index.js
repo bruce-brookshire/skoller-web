8cfbde71c8437e69e09e0640559b7176954994a5
Jon <jonathan@classnavapp.com>
2019-06-06 17:28:23 -0500

    calendar is hidden on viewports less than 720 width (it looks shit otherwise)

--- src/views/components/StudentNav/index.js
+++ src/views/components/StudentNav/index.js
@@ -6,11 +6,29 @@ import NavItem from './NavItem'
 @inject('rootStore')
 @observer
 class StudentNav extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      isMobile: false
+    }
+  }
+
+  componentWillMount () {
+    this.checkForMobile()
+  }
+
   getActivePage () {
     const {
       studentNavStore: { activePage }
     } = this.props.rootStore
     return activePage
+  }
+
+  checkForMobile () {
+    if (window.innerWidth <= 720) {
+      this.setState({isMobile: true})
+    }
   }
 
   render () {
@@ -20,7 +38,7 @@ class StudentNav extends React.Component {
           <NavItem pageName="home" text="Home" />
           <NavItem pageName="classes" text="Classes" />
           <NavItem pageName="tasks" text="Tasks" />
-          <NavItem pageName="calendar" text="Calendar" />
+          {this.state.isMobile ? '' : <NavItem pageName="calendar" text="Calendar" />}
           <NavItem pageName="activity" text="Activity" />
           <NavItem pageName="chat" text="Chat" />
         </div>
