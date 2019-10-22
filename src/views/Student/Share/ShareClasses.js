d7d8d59f1b1be4146528bab01934345e33c43e57
Jon <jonathan@classnavapp.com>
2019-10-22 11:48:30 -0500

    webapp sharing initial commit

---
+++ src/views/Student/Share/ShareClasses.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import SkSelectDropDown from '../../components/SkSelectDropDown'
+import CopyBox from '../../components/CopyBox'
+
+class ShareClasses extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      classSelection: this.props.classes.length > 0 ? this.props.classes[0] : null
+    }
+  }
+
+  selectClassHandler (studentClass) {
+    this.setState({classSelection: studentClass, showClassDropDown: false})
+  }
+
+  renderClassOptions = () => {
+    let classes = this.state.classes
+    classes = classes.filter(c => c.status.id >= 1400)
+    return (
+      <div>
+        {classes.map(studentClass => {
+          return (
+            <div
+              className='sk-share-autocomplete-option'
+              key={studentClass.id}
+              onClick={() => {
+                this.selectClassHandler(studentClass)
+              }}
+              style={{color: studentClass.getColor()}}
+            >
+              {studentClass.name}
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+
+  renderSelect () {
+    return (
+      <div>
+        <div className='sk-share-form-select' onClick={() => this.setState({showClassDropDown: !this.state.showClassDropDown})}>
+          {this.state.classSelection
+            ? <div className='sk-share-form-selection' style={{color: this.state.classSelection.getColor()}}>{this.state.classSelection.name}</div>
+            : <div>Select a class</div>
+          }
+        </div>
+        <SkSelectDropDown
+          optionsMap={() => this.renderClassOptions()}
+          show={this.state.showClassDropDown}
+          toggle={() => this.setState({showClassDropDown: !this.state.showClassDropDown})}
+        />
+      </div>
+    )
+  }
+
+  renderShareMessage () {
+    const name = this.state.classSelection.name
+    return (
+      'Ditch your paper planner. Skoller unlocks academic organization and keeps you on track all year long. Our class ' +
+      name + " is already set up. Sign up through the link you'll automatically be enrolled in our class.\n" +
+      '\n' + this.state.classSelection.enrollment_link
+    )
+  }
+
+  renderShareForm () {
+    return (
+      <div className='sk-share-form'>
+        <h2>Send Link to Classmates</h2>
+        <div className='sk-share-form-section'>
+          <h4>1. Select a class</h4>
+          {this.renderSelect()}
+        </div>
+        <div className='sk-share-form-section'>
+          <h4>2. Click the box to copy the message</h4>
+          <CopyBox
+            longMessage={true}
+            linkValue={this.renderShareMessage()}
+          />
+        </div>
+        <div className='sk-share-form-section'>
+          <h4>3. Paste and share with classmates!</h4>
+          <p>The best ways to share are in a class group text, Blackboard, GroupMe, Facebook, etc.</p>
+        </div>
+      </div>
+    )
+  }
+
+  render () {
+    return (
+      <div></div>
+    )
+  }
+}
+
+ShareClasses.propTypes = {
+  classes: PropTypes.array,
+  user: PropTypes.object
+}
+
+export default ShareClasses
