866933ef1542b1b25551d47a884bc4be05d3abe5
Jon <jonathan@classnavapp.com>
2020-01-15 13:02:56 -0600

    tasks page add assignments

--- src/views/Student/Tasks/index.js
+++ src/views/Student/Tasks/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import {inject, observer} from 'mobx-react'
 import StudentLayout from '../../components/StudentLayout'
 import TasksList from './TasksList'
+import AddAssignment from '../Assignments/AddAssignment'
 
 @inject('rootStore') @observer
 
@@ -13,6 +14,10 @@ class Tasks extends React.Component {
     this.props.rootStore.studentNavStore.location = this.props.location
 
     this.props.rootStore.studentNavStore.setActivePage('tasks')
+
+    this.state = {
+      showAddAssignmentModal: false
+    }
   }
 
   render () {
@@ -22,7 +27,9 @@ class Tasks extends React.Component {
           <div className='tasks-container'>
             <h1>To-Do&apos;s</h1>
             <p className='tasks-sub-heading'>Rest of the semester</p>
+            <div className='tasks-add-new' onClick={() => this.setState({showAddAssignmentModal: true})}><i className='fas fa-plus' /></div>
             <TasksList />
+            {this.state.showAddAssignmentModal && <AddAssignment assignmentParams={{}} closeModal={() => this.setState({showAddAssignmentModal: false})} />}
           </div>
         </div>
       </StudentLayout>
