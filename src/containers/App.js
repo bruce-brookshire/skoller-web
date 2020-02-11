345345819d6c7d79f4683fcac0f87e65f2b63c34
Jon <jonathan@classnavapp.com>
2020-02-11 10:25:19 -0600

    undoing google analytics

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,6 @@ import React from 'react'
 import {inject, observer} from 'mobx-react'
 import Snackbar from '../components/Snackbar'
 import { withRouter } from 'react-router-dom'
-import ReactGA from 'react-ga'
 
 @inject('rootStore') @observer
 class App extends React.Component {
@@ -14,36 +13,36 @@ class App extends React.Component {
 
   render () {
     // GOOGLE ANALYTICS PAGE VIEW LOGIC USING REACT-ROUTER-DOM'S HISTORY OBJECT
-    this.props.history.listen(l => {
-      let sendHit = true
-      let path = l.pathname
+    // this.props.history.listen(l => {
+    //   let sendHit = true
+    //   let path = l.pathname
 
-      let assignmentPath = /\/student\/class\/\d+\/assignments\/\d+/
-      if (path.match(assignmentPath)) {
-        path = '/student/class/classId/assignments/assignmentId/'
-      }
+    //   let assignmentPath = /\/student\/class\/\d+\/assignments\/\d+/
+    //   if (path.match(assignmentPath)) {
+    //     path = '/student/class/classId/assignments/assignmentId/'
+    //   }
 
-      let classPath = /\/student\/class\/\d+/
-      if (path.match(classPath)) {
-        path = '/student/class/classId/'
-      }
+    //   let classPath = /\/student\/class\/\d+/
+    //   if (path.match(classPath)) {
+    //     path = '/student/class/classId/'
+    //   }
 
-      let syllabusToolPath = /\/student\/class\/\d+\/syllabus_tool/
-      if (path.match(syllabusToolPath)) {
-        path = '/student/class/classId/syllabus_tool'
-      }
+    //   let syllabusToolPath = /\/student\/class\/\d+\/syllabus_tool/
+    //   if (path.match(syllabusToolPath)) {
+    //     path = '/student/class/classId/syllabus_tool'
+    //   }
 
-      let hubPath = /\/hub/
-      let adminLoginPath = /\/admin-login/
-      if (path.match(hubPath) || path.match(adminLoginPath)) {
-        sendHit = false
-      }
+    //   let hubPath = /\/hub/
+    //   let adminLoginPath = /\/admin-login/
+    //   if (path.match(hubPath) || path.match(adminLoginPath)) {
+    //     sendHit = false
+    //   }
 
-      if (sendHit) {
-        ReactGA.set({ page: path })
-        ReactGA.pageview(path)
-      }
-    })
+    //   if (sendHit) {
+    //     ReactGA.set({ page: path })
+    //     ReactGA.pageview(path)
+    //   }
+    // })
 
     return (
       <div className='app public'>
