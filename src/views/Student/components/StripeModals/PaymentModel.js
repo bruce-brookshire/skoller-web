5b3123dd7b56f6d13eeec91ffbd1a22d559843d9
Santosh Subedi <santsubcode@gmail.com>
2021-11-11 18:16:26 +0545

    test to live stripe key

--- src/views/Student/components/StripeModals/PaymentModel.js
+++ src/views/Student/components/StripeModals/PaymentModel.js
@@ -7,8 +7,8 @@ import actions from '../../../../actions'
 import { Cookies } from 'react-cookie'
 import { loadStripe } from '@stripe/stripe-js'
 const vm = this
-const stripePromise = loadStripe('pk_test_51JV9OSSGLvMTa3qVnwhFxc03IiK5JOGO94YQufQumo21gTgUAdpvMtEGYH9dgH1BPFrrirHuNbiVbE49gPNHHxIU00WpzV3KLP')
-// const stripePromise = loadStripe('pk_live_51JHvLoGtOURsTxunmypyAUNfbRF4jOahklknp1RTBHhxpy3qEveFU7lCWdrBt4YggE5ytlblCgYYHPPzsLC0Gf8K00NC7FWyoh')
+// const stripePromise = loadStripe('pk_test_51JV9OSSGLvMTa3qVnwhFxc03IiK5JOGO94YQufQumo21gTgUAdpvMtEGYH9dgH1BPFrrirHuNbiVbE49gPNHHxIU00WpzV3KLP')
+const stripePromise = loadStripe('pk_live_51JHvLoGtOURsTxunmypyAUNfbRF4jOahklknp1RTBHhxpy3qEveFU7lCWdrBt4YggE5ytlblCgYYHPPzsLC0Gf8K00NC7FWyoh')
 
 @inject('rootStore') @observer
 class ChangeSchool extends React.Component {
